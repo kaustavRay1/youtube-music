3a46ebff7a89f5f2fda1f03aab56b78b288b40fb
Kaustav Ray <140186859+kaustavRay1@users.noreply.github.com>
2024-08-10 00:43:56 +0530

    Update General.js

--- src/components/General.js
+++ src/components/General.js
@@ -156,13 +156,13 @@ const General = ({onClick}) => {
       </Stack>
      <Box p={2}>
      <Stack direction={"row"} sx={{overflow:"auto", overflowY:"hidden",position:"relative","&::-webkit-scrollbar":{width:4,height:9},"&::-webkit-scrollbar-thumb":{background:"black",borderRadius:4,},"&::-webkit-scrollbar-thumb:hover":{background:"red",borderRadius:4,},}}>
-     <Stack direction={"column"} spacing={1} sx={{minWidth:"25em",}}>
+     <Stack direction={"column"} spacin4em2} sx={{minWidth:"25em",}}>
      {arrayDataItems3}
       </Stack>
-      <Stack direction={"column"} spacing={1} sx={{minWidth: "25em",}}>
+      <Stack direction={"column"} spacing={2} sx={{minWidth: "25em",}}>
       {arrayDataItems4}
       </Stack>
-      <Stack direction={"column"} spacing={1} sx={{minWidth: "25em",}}>
+      <Stack direction={"column"} spacing={2} sx={{minWidth: "25em",}}>
       {arrayDataItems5}
       </Stack>
       </Stack>
