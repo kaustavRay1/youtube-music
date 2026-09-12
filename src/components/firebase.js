99c015007a0f05cc2dd9aeeb8fee05ef3f1f30cc
kaustavRay1 <kaustavray2004@gmail.com>
2026-09-12 20:51:55 +0530

    firebase

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -9,14 +9,12 @@ import {getAuth} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBylJyC657TXFdV3KF6tiBJk_ANXtgiBdY",
-  authDomain: "yt-music-clone-54d4f.firebaseapp.com",
-  databaseURL: "https://yt-music-clone-54d4f-default-rtdb.firebaseio.com",
-  projectId: "yt-music-clone-54d4f",
-  storageBucket: "yt-music-clone-54d4f.appspot.com",
-  messagingSenderId: "47256296388",
-  appId: "1:47256296388:web:63752125ad60ee35b26291",
-  measurementId: "G-CEYB8JEXY5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
